ce511ae35a533fd7463b26dd2d1082ce781b8a7c
rorycerbus <31298664+rorycerbus@users.noreply.github.com>
2018-04-21 15:11:01 +0900

    Add files via upload
    This MATLAB code can be used to import the velocity time series created using the OpenFOAM (post)Processing utility "probe". The location of the probes are indicated in a file, e.g. "probes" placed in the system folder. The function must also be invoked in the controlDict file, also in the system folder. Once the solver starts, a new folder is created called "postProcessing", which will in turn contain folders of the (post)Processing utilities invoked in controlDict. Inside the "probes" folder will be one or more folders, each of which is the starting time of the runs. If run only once, starting from "0", then there will only be a "0" folder. Inside each of these time folders will be a file called "p" (pressure) and "U" (velocity) and/or whatever else is indicated in the "probes" file in the system folder. This MATLAB script can be used to import the velocity time series data from "U", and it could easily be modified to import from "p" as well.

---
+++ OpenFOAM_importProbeVelocity.m
@@ -0,0 +1,32 @@
+% script to retrieve OpenFoam p and U data and interpolate
+
+clc; clear all
+
+option = 1; % 0 is no interpolate, 1 is interpolate
+directory = '/Users/soda/Documents/Physics/OpenFOAM/pipe/iris/Re3000_100D_probes/';
+filename = 'U';
+probes = 6; % (remember that OpenFOAM numbering starts from 0!)
+% the probe output is a text file with N rows and M columns
+% N = headers plus all the times
+% M = number of probes times 3 plus 1 for time
+% the following makes a string of '%f' to import each as floating point numbers
+probestr = '%f %f %f %f'; % first is time
+for i = 2:probes
+    probestr = ([probestr ' %f %f %f']);
+end
+
+% import velocity
+fileID = fopen([directory num2str(times(1)) '/' filename],'r');
+U = textscan(fileID,probestr,'headerlines',probes+2,... %the number of headers is 2 plus a list of the probes
+    'delimiter',' ()','collectoutput',1,'multipledelimsasone',true);
+fclose(fileID);
+U = cell2mat(U); % convert the cell to a matrix
+t = U(:,1); % time is the first column
+for i = 1:probes
+    ux(:,i) = U(:,(i-1)*3+2); % x components
+    uy(:,i) = U(:,(i-1)*3+3); % y component
+    uz(:,i) = U(:,(i-1)*3+4); % z components
+end
+
+% remember that the times may be unequally spaced depending on the time
+% control settings in controlDict
